40cec1d0021b4b2bce46dc0017778cf0b0a5e1c8
Thomas Horrobin <thomas.horrobin@nzx.com>
2016-11-24 08:00:54 +1300

    made recursive function

--- Main.hs
+++ Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn $ show $ addarray [4, 8, 4, 8]
+
+natSum 0 = 0
+natSum n = n + natSum (n - 1)
+
+addarray :: (Num a) => [a] -> a
+addarray [] = 0
+addarray aa = aa!!0 + ( addarray $ drop 1 aa )
